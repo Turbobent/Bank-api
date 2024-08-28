aa98c9663749edf6441153b7330aead950caf28e
Turbobent <kasp728h@edu.mercantec.dk>
2024-08-28 09:43:42 +0200

    made route service

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\TransactionController;
+use App\Http\Controllers\Api\V1\AccountController;
 
-Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//api/v1
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
+// API v1 routes
+Route::group(['prefix' => 'v1'], function() {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('transactions', TransactionController::class);
     Route::apiResource('accounts', AccountController::class);

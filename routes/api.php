ca36593015d2d069c584cea7af36f71397eff1da
Turbobent <kasp728h@edu.mercantec.dk>
2024-08-29 08:11:48 +0200

    Revert "made route service"
    This reverts commit aa98c9663749edf6441153b7330aead950caf28e.

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\CustomerController;
-use App\Http\Controllers\Api\V1\TransactionController;
-use App\Http\Controllers\Api\V1\AccountController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
     return $request->user();
 });
 
-// API v1 routes
-Route::group(['prefix' => 'v1'], function() {
+//api/v1
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function() {
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('transactions', TransactionController::class);
     Route::apiResource('accounts', AccountController::class);
